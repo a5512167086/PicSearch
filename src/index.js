54ea3d2ef8035ea9db12a2b783e86de9ecbe6197
Shin <209410157@gms.tku.edu.tw>
2021-12-19 21:42:52 +0800

    feat(gh-pages): deploy version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/PicSearch">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
